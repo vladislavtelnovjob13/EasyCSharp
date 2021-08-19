93ef3e8be352761942e58f1fb29c95c7d68919b2
Владислав <vladislav.telnov.joboffer@gmail.com>
2021-08-19 22:31:12 +0300

    Education. Task №3.3.2. Create and use a method

---
+++ EasyCSharp/Properties_Shed.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EasyCSharp
+{
+    class Properties_Shed
+    {
+
+        static void Main(string[] args)
+        {
+            Shed myFirstShed = new Shed();
+            myFirstShed.Height = 10;
+            myFirstShed.Width = 20;
+            myFirstShed.Lengthwise = myFirstShed.Width;
+
+            Console.WriteLine("Высота =" + myFirstShed.Height);
+            Console.WriteLine("Ширина =" + myFirstShed.Width);
+            Console.WriteLine("Глубина =" + myFirstShed.Lengthwise);
+
+            int size = myFirstShed.GetSize();
+            Console.WriteLine("Объем =" + size);
+            Console.WriteLine("Объем =" + myFirstShed.GetSize());
+
+
+            Console.ReadLine();
+
+            Shed shed = new Shed();
+            shed.Height = 1;
+            shed.Width = 5;
+            shed.Lengthwise = 10;
+            shed.ExpandSize(10, 13, 14);
+        }
+    }
+
+}
---
+++ EasyCSharp/Shed.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EasyCSharp
+{
+    class Shed
+    {
+        int width;
+        int height;
+
+        public int Width
+        {
+            get { return width; }
+            set { width = value; }
+        }
+
+        public int Height
+        {
+            get { return height; }
+            set { height = value; }
+        }
+
+        public int Lengthwise { get; set; }
+
+        public int GetSize()
+        {
+            int size = width * height * Lengthwise;
+            return size;
+
+            //return width * height * Lengthwise;
+        }
+
+        public void ExpandSize(int x, int y, int h) 
+        {
+            Width += x;
+            Lengthwise += y;
+            Height += h;
+
+        }
+    }
+}
